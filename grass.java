4ac8d510dd9452a48789a804032cfd775cec6ce1
agus makmun <summon.agus@gmail.com>
2015-03-24 22:23:11 +0700

    grass will be eat with kumbang

---
+++ grass.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Ini adalah merupakan untuk grass/rumput (tidak ada kondisi apapun).
+ * 
+ * @author (Summon Agus) 
+ * @blog (bloggersmart.net)
+ * @version (V.1.0)
+ */
+
+public class grass extends Actor
+{
+    /**
+     * Act - do whatever the grass wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
